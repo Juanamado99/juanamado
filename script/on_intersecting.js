512a58be7a9daa48843cdac5526f7f70b0763860
JALL <jhonnyrex3@outlook.com>
2026-01-29 15:09:15 +0100

    Attach scroll listener only when hero is visible

--- script/on_intersecting.js
+++ script/on_intersecting.js
@@ -1,5 +1,16 @@
 const fadeElements = document.querySelectorAll('.fade-block');
+let isListenerCreated = false;
+let hasVisible = false;
+
+const onScroll = () => {
+
+
+    
+    manageScrollListener();
+};
+
 const observer = new IntersectionObserver((entries) => {
+    manageScrollListener();
     entries.forEach(entry => {
         const el = entry.target;
         if(entry.isIntersecting){
@@ -11,4 +22,18 @@ const observer = new IntersectionObserver((entries) => {
         }
     });
 }, { threshold: 0.2 });
+
 fadeElements.forEach(el => observer.observe(el));
+
+function manageScrollListener() {
+    const a = Array.from(fadeElements).map(item => item["className"]);
+    hasVisible = a.some(i => i.split(" ").includes("visible"));
+    if (!isListenerCreated && hasVisible) {
+        isListenerCreated = true;
+        document.addEventListener("scroll", onScroll);
+    }
+    if (isListenerCreated && !hasVisible) {
+        isListenerCreated = false;
+        document.removeEventListener("scroll", onScroll);
+    }
+}
