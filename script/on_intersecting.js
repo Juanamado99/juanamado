57843c3f6765cc0f361dea63dd6790e379929de1
JALL <jhonnyrex3@outlook.com>
2026-02-05 17:20:34 +0100

    Add scroll-based hero visibility states

--- script/on_intersecting.js
+++ script/on_intersecting.js
@@ -1,10 +1,44 @@
 const fadeElements = document.querySelectorAll('.fade-block');
+const arrayElements = Array.from(fadeElements);
+const elementsClassList = arrayElements.map(item => item["className"]);
 let isListenerCreated = false;
 let hasVisible = false;
 
 const onScroll = () => {
+    const page = document.body.parentNode;
+    const posPercentage = page.scrollTop/(page.scrollHeight-page.clientHeight);
 
+    if (posPercentage < 0.05 || arrayElements[0].className.includes("outscreen")) {
+        arrayElements[0].classList.remove('visible');
+        arrayElements[0].classList.add('invisible');
+    } else {
+        arrayElements[0].classList.remove('invisible');
+        arrayElements[0].classList.add('visible');
+    }
 
+    if (posPercentage < 0.1 || arrayElements[1].className.includes("outscreen")) {
+        arrayElements[1].classList.remove('visible');
+        arrayElements[1].classList.add('invisible');
+    } else {
+        arrayElements[1].classList.remove('invisible');
+        arrayElements[1].classList.add('visible');
+    }
+
+    if (posPercentage < 0.15 || arrayElements[2].className.includes("outscreen")) {
+        arrayElements[2].classList.remove('visible');
+        arrayElements[2].classList.add('invisible');
+    } else {
+        arrayElements[2].classList.remove('invisible');
+        arrayElements[2].classList.add('visible');
+    }
+
+    if (posPercentage < 0.2 || arrayElements[3].className.includes("outscreen")) {
+        arrayElements[3].classList.remove('visible');
+        arrayElements[3].classList.add('invisible');
+    } else {
+        arrayElements[3].classList.remove('invisible');
+        arrayElements[3].classList.add('visible');
+    }
     
     manageScrollListener();
 };
@@ -14,11 +48,11 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         const el = entry.target;
         if(entry.isIntersecting){
-            el.classList.add('visible');
-            el.classList.remove('out');
+            el.classList.add('onscreen');
+            el.classList.remove('outscreen');
         } else {
-            el.classList.remove('visible');
-            el.classList.add('out');
+            el.classList.remove('onscreen');
+            el.classList.add('outscreen');
         }
     });
 }, { threshold: 0.2 });
@@ -26,8 +60,7 @@ const observer = new IntersectionObserver((entries) => {
 fadeElements.forEach(el => observer.observe(el));
 
 function manageScrollListener() {
-    const a = Array.from(fadeElements).map(item => item["className"]);
-    hasVisible = a.some(i => i.split(" ").includes("visible"));
+    hasVisible = elementsClassList.some(i => i.split(" ").includes("onscreen"));
     if (!isListenerCreated && hasVisible) {
         isListenerCreated = true;
         document.addEventListener("scroll", onScroll);
