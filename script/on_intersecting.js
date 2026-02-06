3824ef2815c350bdd30d777a99aaa36e3801b399
JALL <jhonnyrex3@outlook.com>
2026-02-06 13:09:19 +0100

    Remove unused hero classes and obsolete listeners

--- script/on_intersecting.js
+++ script/on_intersecting.js
@@ -1,58 +1,39 @@
 const fadeElements = document.querySelectorAll('.fade-block');
 const arrayElements = Array.from(fadeElements);
-const elementsClassList = arrayElements.map(item => item["className"]);
 let isListenerCreated = false;
-let hasVisible = false;
-let animationEnabled = false;
-
-const observer = new IntersectionObserver((entries) => {
-    manageScrollListener();
-    entries.forEach(entry => {
-        const el = entry.target;
-        if(entry.isIntersecting){
-            el.classList.add('onscreen');
-            el.classList.remove('outscreen');
-            if(!animationEnabled) {
-                animationEnabled = true;
-            }
-        } else {
-            el.classList.remove('onscreen');
-            el.classList.add('outscreen');
-        }
-    });
-}, { threshold: 0.2 });
-
-fadeElements.forEach(el => observer.observe(el));
+let isOnLoad = false;
 
 function manageScrollListener() {
-    hasVisible = elementsClassList.some(i => i.split(" ").includes("onscreen"));
-    if (!isListenerCreated && hasVisible) {
+    const elementsClassList = arrayElements.map(item => item["className"]);
+    isOnLoad = elementsClassList.some(i => i.split(" ").includes("preload"));
+
+    if (!isListenerCreated && isOnLoad) {
         isListenerCreated = true;
         document.addEventListener("scroll", onScroll);
     }
-    if (isListenerCreated && !hasVisible) {
+    if (isListenerCreated && !isOnLoad) {
+        
         isListenerCreated = false;
         document.removeEventListener("scroll", onScroll);
     }
 }
 
 const onScroll = () => {
-    if(!animationEnabled) return;
-
     const page = document.body.parentNode;
     const posPercentage = page.scrollTop/(page.scrollHeight-page.clientHeight);
     
     for(i = 0; i<=4; i++) {
-        toggle(arrayElements[i], posPercentage < 0.05);
+        toggle(arrayElements[i], posPercentage > 0.05);
     }
     
     manageScrollListener();
 };
 
-function toggle(el, shouldHide) {
-    if (!shouldHide && el.classList.contains("onscreen")) {
-        el.classList.remove("novisible");
+function toggle(el, shouldShow) {
+    if (shouldShow && el.classList.contains("preload")) {
         el.classList.remove("preload");
         el.classList.add("visible");
     }
 }
+
+manageScrollListener();
